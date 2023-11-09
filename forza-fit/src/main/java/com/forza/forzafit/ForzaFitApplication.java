ccb3c6adae48fe20809342730768c58bc8cb9177
Anghelo Ortiz <17k1ra2003@gmail.com>
2023-11-09 12:49:34 -0500

    feat: Proyecto inicial en Spring Boot - ForzaFit

---
+++ forza-fit/src/main/java/com/forza/forzafit/ForzaFitApplication.java
@@ -0,0 +1,13 @@
+package com.forza.forzafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForzaFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForzaFitApplication.class, args);
+    }
+
+}
